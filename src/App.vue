d8b8601d09e2c5997429e9c1cfa728f6026f1c46
Joshua Luce <joshuabluce@protonmail.com>
2020-06-13 23:23:27 -0500

    Progress?

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ god }}</h1>
+    <ul>
+      <li v-for="item in items" :key="item.name">{{ item.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      god: 'Test',
+      items: []
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let app = new Vue({
+    data: {
+        god: '',
+        items: []
+    },
+    render: h => h(App)
+}).$mount('#app');
+
+import SmiteTeamGenerator from 'smite-builder';
+let stg = new SmiteTeamGenerator();
+stg.getLists(() => {
+    console.log(stg.lists);
+    let player = stg.generateTeam({size: 1}).getPlayer(0);
+    app.god = player.god.name;
+    app.items = player.build.items;
+});
